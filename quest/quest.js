552cdef182a06ecb37987022766988f3067508a1
Chase Crowder <gcrowder93@gmail.com>
2021-10-28 09:42:59 -0700

    last errors

--- quest/quest.js
+++ quest/quest.js
@@ -10,7 +10,7 @@ import { loadProfile } from '../utils.js';
 const searchParams = new URLSearchParams(window.location.search);
 const questData = findById(quests, searchParams.get('id'));
 // const title = document.getElementById('quest-title');
-title.textContent = questData.title;
+// title.textContent = questData.title;
 const img = document.getElementById('quest-image');
 img.src = `../assets/${questData.image}`;
 const description = document.getElementById('quest-description');
